bcc669bd6e605d6f1254dab39857bf56cb6ebe05
YehonatanMMedina <123869617+YehonatanMMedina@users.noreply.github.com>
2023-01-30 14:33:26 +0200

    added interface(in the regular script)

--- FunctionClass.py
+++ FunctionClass.py
@@ -61,6 +61,7 @@ class Function:
 
             if isComplexFunc:
                 self.func1 = ''
+           
     def startingIntLength(self):
         if self.function[0].isnumeric():
             i=0
@@ -136,6 +137,17 @@ class Function:
             return "(" + Func2.findDerivative() + ") / (2*sqrt("+Func2.function+"))"
         if self.operation == "exp":
             return "("+Func2.findDerivative() + ") / (2*sqrt(" + Func2.function + "))"
-fun = Function("exp(sqrt(sin(6*x)))")
-print(fun.findDerivative())
+#fun = Function("")
+#print(fun.findDerivative())
 
+print("Welcome to my numeric analasis project")
+function = input("Enter A Function:")
+f= Function(function)
+action=int(input("What do you want to find: 1-value, 2-derivative"))
+if action == 1:
+    x = input("Enter x coordinate")
+    print("The value of the function at x=" +str(x)+ " is:")
+    print(f.calcvalue(x))
+if(action == 2):
+    print("the derivative of the function is:")
+    print(f.findDerivative())
