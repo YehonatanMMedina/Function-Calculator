bcd141755d98119ca9816df95c14baecae0a614c
YehonatanMMedina <123869617+YehonatanMMedina@users.noreply.github.com>
2023-01-30 15:02:07 +0200

    to delete

--- Interface.py
+++ Interface.py
@@ -1,2 +1,13 @@
 import FunctionClass
 
+print("Welcome to my numeric analasis project")
+function = input("Enter A Function:")
+f= Function(function)
+action=int(input("What do you want to find: 1-value, 2-derivative"))
+if action == 1:
+    x = input("Enter x coordinate")
+    print("The value of the function at x=" +str(x)+ " is:")
+    print(f.calcvalue(x))
+if(action == 2):
+    print("the derivative of the function is:")
+    print(f.findDerivative())
